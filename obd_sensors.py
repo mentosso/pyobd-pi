ade2eb48dc94c5901592542d12bd36e9a61a79ba
mikes01 <mikolaj0grygiel@gmail.com>
2015-08-11 21:00:15 +0200

    PIDs 013C - 015F

--- obd_sensors.py
+++ obd_sensors.py
@@ -72,6 +72,11 @@ def fuel_trim_percent(code):
     #return (code - 128.0) * 100.0 / 128
     return (code - 128) * 100 / 128
 
+def fuel_trim_percent_2(code):
+    code = hex_to_int(code[2:4])
+    #return (code - 128.0) * 100.0 / 128
+    return (code - 128) * 100 / 128
+
 def dtc_decrypt(code):
     #first byte is byte after PID and without spaces
     num = hex_to_int(code[:2]) #A byte
@@ -147,6 +152,31 @@ def equivalence_ratio(code):
 def vapor_press(code):
     return mil(code) / 4
 
+def catalyst_temperature(code):
+    return mil(code) / 10 - 40
+
+def ctrl_module_vol(code):
+    return mil(code) / 1000
+
+def abs_load_val(code):
+    return mil(code) / 100 * 255
+
+def amb_air_temp(code):
+    code = hex_to_int(code)
+    return code - 40
+
+def abs_vapor_press(code):
+    return mil(code) / 200
+
+def vapor_press(code):
+    return mil(code) - 32767
+
+def fuel_inj_timing(code):
+    return (mil(code) - 26880)/128
+
+def fuel_rate(code):
+    return mil(code) / 100
+
 class Sensor:
     def __init__(self, shortName, sensorName, sensorcommand, sensorValueFunction, u):
         self.shortname = shortName
@@ -188,7 +218,7 @@ SENSORS = [
     Sensor("o2_sensor_position_b"  , "Loc of O2 sensor" 			                                            , "011D" , cpass            ,""       ),
     Sensor("aux_input"             , "Aux input status"				                                            , "011E" , cpass            ,""       ),
     Sensor("engine_time"           , "Engine Start MIN"				                                            , "011F" , sec_to_min       ,"min"    ),
-    
+
     Sensor("pids2"                 , "Supported PIDs 21-40"		                                                , "0120" , hex_to_bitstring ,""       ),
     Sensor("mil"                   , "Distance traveled with malfunction indicator lamp (MIL) on"				, "0121" , mil              ,"km"     ),
     Sensor("rel_fuel_press"        , "Fuel Rail Pressure (relative to manifold vacuum)"				            , "0122" , rel_fuel_press   ,"kPa"    ),
@@ -222,7 +252,67 @@ SENSORS = [
     Sensor("o2s7_2"                , "O2S7_WR_lambda(1) Equivalence Ratio 2"			                        , "013A" , equivalence_ratio,""       ),
     Sensor("o2s8_2"                , "O2S8_WR_lambda(1) Equivalence Ratio 2"			                        , "013B" , equivalence_ratio,""       ),
 
-    Sensor("engine_mil_time"       , "Engine Run MIL"				, "014D" , sec_to_min       ,"min"    ),
+    Sensor("b1s1_temp"             , "Catalyst Temperature Bank 1, Sensor 1"			                        , "013C" , catalyst_temperature,""    ),
+    Sensor("b2s1_temp"             , "Catalyst Temperature Bank 2, Sensor 1"			                        , "013D" , catalyst_temperature,""    ),
+    Sensor("b1s2_temp"             , "Catalyst Temperature Bank 1, Sensor 2"			                        , "013E" , catalyst_temperature,""    ),
+    Sensor("b2s2_temp"             , "Catalyst Temperature Bank 2, Sensor 2"			                        , "013F" , catalyst_temperature,""    ),
+
+    Sensor("pids3"                 , "Supported PIDs 41-60"		                                                , "0140" , hex_to_bitstring ,""       ),
+
+    Sensor("monitor_status"        , "Monitor status this drive cycle"		                                    , "0141" , hex_to_bitstring ,""       ),
+    Sensor("ctrl_module_vol"       ,"Control module voltage"		                                            , "0142" , ctrl_module_vol  ,""       ),
+    Sensor("abs_load_val"          , "Absolute load value"		                                                , "0143" , abs_load_val        ,""    ),
+    Sensor("fuel_air_ratio"        , "Fuel/Air commanded equivalence ratio"		                                , "0144" , equivalence_ratio ,""      ),
+
+    Sensor("rel_throttle"          , "Relative throttle position"				                                , "0145" , percent_scale,"%"          ),
+    Sensor("amb_air_temp"          , "Ambient air temperature"				                                    , "0146" , amb_air_temp,"C"           ),
+    Sensor("abs_throttle_b"        , "Absolute throttle position B"				                                , "0147" , percent_scale,"%"          ),
+    Sensor("abs_throttle_c"        , "Absolute throttle position C"				                                , "0148" , percent_scale,"%"          ),
+    Sensor("abs_throttle_d"        , "Absolute throttle position D"				                                , "0149" , percent_scale,"%"          ),
+    Sensor("abs_throttle_e"        , "Absolute throttle position E"				                                , "014A" , percent_scale,"%"          ),
+    Sensor("abs_throttle_f"        , "Absolute throttle position F"				                                , "014B" , percent_scale,"%"          ),
+    Sensor("throttle_actuator"     , "Commanded throttle actuator"			    	                            , "014C" , percent_scale,"%"          ),
+
+    Sensor("engine_mil_time"       , "Engine Run MIL"				                                            , "014D" , mil       ,"min"            ),
+    Sensor("cleared_trouble_time"  , "Time since trouble codes cleared"				                            , "014E" , mil       ,"min"            ),
+
+    Sensor("max_eq_ratio"          , "Maximum value for equivalence ratio"				                        , "014F" , cpass       ,""            ),
+    Sensor("max_o2_vol"            , "Maximum value for oxygen sensor voltage"				                    , "014F" , cpass       ,""            ),
+    Sensor("max_o2_curr"           , "Maximum value for oxygen sensor current"				                    , "014F" , cpass       ,""            ),
+    Sensor("max_in_press"          , "Maximum value for intake manifold absolute pressure"				        , "014F" , cpass       ,""            ),
+
+    Sensor("max_airflow_a"          , "Maximum value for air flow rate from mass air flow sensor A"				, "0150" , cpass       ,""            ),
+    Sensor("max_airflow_b"          , "Maximum value for air flow rate from mass air flow sensor B"		    	, "0150" , cpass       ,""            ),
+    Sensor("max_airflow_c"          , "Maximum value for air flow rate from mass air flow sensor C"		    	, "0150" , cpass       ,""            ),
+    Sensor("max_airflow_d"          , "Maximum value for air flow rate from mass air flow sensor D"				, "0150" , cpass       ,""            ),
+
+    Sensor("fuel_type"              , "Fuel Type"                                               				, "0151" , cpass       ,""            ),
+    Sensor("ethanol_fuel_per"       , "Ethanol fuel %"                                               			, "0152" , percent_scale,"%"          ),
+    Sensor("abs_vapor_press"        , "Absolute Evap system Vapor Pressure"                                     , "0153" , abs_vapor_press,"kPa"          ),
+    Sensor("vapor_press"            , "Evap system vapor pressure"                                              , "0154" , vapor_press,"Pa"          ),
+
+    Sensor("short_term_b1"          , "Short term secondary oxygen sensor trim bank 1"                          , "0155" , fuel_trim_percent,"%"          ),
+    Sensor("short_term_b3"          , "Short term secondary oxygen sensor trim bank 3"                          , "0155" , fuel_trim_percent_2,"%"          ),
+    Sensor("long_term_b1"           , "Long term secondary oxygen sensor trim bank 1"                           , "0156" , fuel_trim_percent,"%"          ),
+    Sensor("long_term_b3"           , "Long term secondary oxygen sensor trim bank 3"                           , "0156" , fuel_trim_percent_2,"%"          ),
+    Sensor("short_term_b2"          , "Short term secondary oxygen sensor trim bank 2"                          , "0157" , fuel_trim_percent,"%"          ),
+    Sensor("short_term_b4"          , "Short term secondary oxygen sensor trim bank 4"                          , "0157" , fuel_trim_percent_2,"%"          ),
+    Sensor("long_term_b2"           , "Long term secondary oxygen sensor trim bank 2"                           , "0158" , fuel_trim_percent,"%"          ),
+    Sensor("long_term_b4"           , "Long term secondary oxygen sensor trim bank 4"                           , "0158" , fuel_trim_percent_2,"%"          ),
+
+    Sensor("abs_fuel_press"         , "Fuel rail pressure (absolute)"				                            , "0159" , dir_fuel_press   ,"kPa"    ),
+    Sensor("accel_pedal_pos"        , "Relative accelerator pedal position"				                        , "015A" , percent_scale   ,"%"    ),
+    Sensor("hybrid_battery"         , "Hybrid battery pack remaining life"				                        , "015B" , percent_scale   ,"%"    ),
+    Sensor("oil_temp"               , "Engine oil temperature"	            			                        , "015C" , amb_air_temp   ,"C"    ),
+    Sensor("fuel_inj_timing"        , "Fuel injection timing"	            			                        , "015D" , fuel_inj_timing   ,"st"    ),
+    Sensor("fuel_rate"              , "Engine fuel rate"	            			                            , "015E" , fuel_rate   ,"L/h"    ),
+    Sensor("fuel_inj_timing"        , "Emission requirements to which vehicle is designed"	            		, "015F" , cpass   ,""    ),
+
+
+
+
+
+
     ]
      
     
